2c32492a9116a9ba3b555a97257c12ed798f1a35
ngustavo80 <ngustavo80@hotmail.com>
2025-03-09 18:07:13 -0300

    config:react-router-dom

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,5 @@
+export function Dashboard() {
+  return (
+    <h1 className="flex h-screen items-center justify-center">Dashboard</h1>
+  )
+}
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,3 @@
+export function SignIn() {
+  return <h1 className="flex h-screen items-center justify-center">SignIn</h1>
+}

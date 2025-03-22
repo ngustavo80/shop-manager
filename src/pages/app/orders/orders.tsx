5ab7939692a8edfd58cfb8d924b9f63e0c9466fd
ngustavo80 <ngustavo80@hotmail.com>
2025-03-22 06:35:53 -0300

    feat:orders-filter

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,16 +1,15 @@
-import { ArrowRight, Search, X } from 'lucide-react'
 import { SuperSEO } from 'react-super-seo'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+
+import { OrdersTableFilters } from './orders-table-filters'
+import { OrdersTableRow } from './orders-table-row'
 
 export function Orders() {
   return (
@@ -21,10 +20,7 @@ export function Orders() {
       </div>
 
       <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
+        <OrdersTableFilters />
 
         <div className="rounded-md border border-border">
           <Table>
@@ -43,46 +39,7 @@ export function Orders() {
 
             <TableBody>
               {Array.from({ length: 15 }).map((_, i) => {
-                return (
-                  <TableRow key={i}>
-                    <TableCell>
-                      <Button size="xs" variant="outline">
-                        <Search className="h-3 w-3" />
-                        <span className="sr-only">Detalhes do pedido</span>
-                      </Button>
-                    </TableCell>
-                    <TableCell className="font-mono text-xs font-medium">
-                      fcewfgwefewfewf
-                    </TableCell>
-                    <TableCell className="text-muted-foreground">
-                      há 15 minutos
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400" />
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell className="font-medium">
-                      Gustavo do Nascimento Almeida
-                    </TableCell>
-                    <TableCell className="font-medium">R$ 190,00</TableCell>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <ArrowRight />
-                        Aprovar
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <Button variant="ghost" size="xs">
-                        <X className="mr-2 h-3 w-3" />
-                        Cancelar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                )
+                return <OrdersTableRow key={i} />
               })}
             </TableBody>
           </Table>
